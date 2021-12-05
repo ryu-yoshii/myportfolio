3e2043a9790ed8a62c19f60fc2b43e4ff05fe1d0
ryu-yoshii <dgy15044@gmail.com>
2021-12-05 16:04:18 +0900

    firebase導入成功

--- src/main.js
+++ src/main.js
@@ -2,18 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// import firebase from 'firebase'
-// const config = {
-//   apiKey: "AIzaSyA1Cw7U-3JkyKfTXyE1TssDjmaiDMA_mJc",
-//   authDomain: "portfolio-e51d6.firebaseapp.com",
-//   projectId: "portfolio-e51d6",
-//   storageBucket: "portfolio-e51d6.appspot.com",
-//   messagingSenderId: "509447176150",
-//   appId: "1:509447176150:web:1b89e7da62beb0dbb8ee77",
-//   measurementId: "G-FFM8LPYZEX"
-// };
-// firebase.initializeApp(config);
+import axios from "axios";
+// import firebase from "firebase/app";
+import firebase from "firebase/compat/app";
+const config = {
+  apiKey: "AIzaSyA1Cw7U-3JkyKfTXyE1TssDjmaiDMA_mJc",
+  authDomain: "portfolio-e51d6.firebaseapp.com",
+  projectId: "portfolio-e51d6",
+  storageBucket: "portfolio-e51d6.appspot.com",
+  messagingSenderId: "509447176150",
+  appId: "1:509447176150:web:1b89e7da62beb0dbb8ee77",
+  measurementId: "G-FFM8LPYZEX"
+};
+firebase.initializeApp(config);
 
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
